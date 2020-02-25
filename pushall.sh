406686bf47f008b28706d3ac83871d73673bfb78
haigangx <754657908@qq.com>
2020-02-25 18:14:31 +0800

    auto commit

--- pushall.sh
+++ pushall.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 x=`ls`
 for i in $x; do
-  echo $i;
+  echo "";
+  echo "------$i-----";
   if [ -d "$i" ]; then 
     cd $i;
     if [ -e ".git" ]; then
       git add --all;
       git commit -m "auto commit";
       git push origin master;
-      echo "$i push finished";
+      echo "------$i push finished----";
+
     fi
     cd ..;
   fi
